0eefb9d687d4b5b35df386b5d858561ec6327235
Patrice <patrice@ponytech.net>
2019-05-27 17:58:29 +0200

    GET method is the default

--- appstoreconnect/api.py
+++ appstoreconnect/api.py
@@ -8,10 +8,12 @@ from enum import Enum
 ALGORITHM = 'ES256'
 BASE_API = "https://api.appstoreconnect.apple.com"
 
+
 class HttpMethod(Enum):
 	GET = 1
 	POST = 2
 	PATCH = 3
+
 
 class Api:
 
@@ -31,7 +33,7 @@ class Api:
 		return jwt.encode({'iss': self.issuer_id, 'exp': exp, 'aud': 'appstoreconnect-v1'}, key,
 		                   headers={'kid': self.key_id, 'typ': 'JWT'}, algorithm=ALGORITHM).decode('ascii')
 
-	def _api_call(self, route, method, post_data):
+	def _api_call(self, route, method=HttpMethod.GET, post_data=None):
 		headers = {"Authorization": "Bearer %s" % self.token}
 		url = "%s%s" % (BASE_API, route)
 		r = {}
@@ -45,36 +47,36 @@ class Api:
 			headers["Content-Type"] = "application/json"
 			r = requests.patch(url=url, headers=headers, data=json.dumps(post_data))
 
-		contentType = r.headers['content-type']
+		content_type = r.headers['content-type']
 
-		if contentType == "application/json":
+		if content_type == "application/json":
 			return r.json()
 		else:
-			if r.status_code not in range(200,299):
+			if r.status_code not in range(200, 299):
 				print("Error [%d][%s]" % (r.status_code, r.content))
 
 	#apps
 
 	def apps(self):
-		return self._api_call("/v1/apps", HttpMethod.GET, None)
+		return self._api_call("/v1/apps")
 
 	def app_for_sku(self, sku):
-		return self._api_call("/v1/apps?filter[sku]=" + sku, HttpMethod.GET, None)
+		return self._api_call("/v1/apps?filter[sku]=" + sku)
 
 	#users
 
 	def users(self):
-		return self._api_call("/v1/users", HttpMethod.GET, None)
+		return self._api_call("/v1/users")
 
 	#userInvitations
 
 	def user_invitations(self):
-		return self._api_call("/v1/userInvitations", HttpMethod.GET, None)
+		return self._api_call("/v1/userInvitations")
 
 	#betaGroups
 
 	def beta_groups(self, app_id):
-		return self._api_call("/v1/apps/" + app_id + "/betaGroups", HttpMethod.GET, None)
+		return self._api_call("/v1/apps/" + app_id + "/betaGroups")
 
 	def create_beta_group(self, group_name, app_id):
 		post_data = {'data': {'attributes': {'name': group_name}, 'relationships': {'app': {'data': {'id': app_id, 'type': 'apps'}}}, 'type': 'betaGroups'}}
@@ -82,7 +84,7 @@ class Api:
 		return self._api_call("/v1/betaGroups", HttpMethod.POST, post_data)
 
 	def beta_group_info(self, beta_group_id):
-		return self._api_call("/v1/betaGroups/" + beta_group_id, HttpMethod.GET, None)
+		return self._api_call("/v1/betaGroups/" + beta_group_id)
 
 	def add_build_to_beta_group(self, beta_group_id, build_id):
 		post_data = {'data': [{ 'id': build_id, 'type': 'builds'}]}
@@ -92,7 +94,7 @@ class Api:
 	#betaTesters
 
 	def beta_testers(self):
-		return self._api_call("/v1/betaTesters", HttpMethod.GET, None)
+		return self._api_call("/v1/betaTesters")
 
 	def create_beta_tester(self, beta_group_id, email, first_name, last_name):
 		post_data = {'data': {'attributes': {'email': email, 'firstName': first_name, 'lastName': last_name}, 'relationships': {'betaGroups': {'data': [{ 'id': beta_group_id ,'type': 'betaGroups'}]}}, 'type': 'betaTesters'}}
@@ -100,18 +102,18 @@ class Api:
 		return self._api_call("/v1/betaTesters", HttpMethod.POST, post_data)
 
 	def beta_group_beta_testers(self, beta_group_id):
-		return self._api_call("/v1/betaGroups/" + beta_group_id + "/betaTesters", HttpMethod.GET, None)
+		return self._api_call("/v1/betaGroups/" + beta_group_id + "/betaTesters")
 
 	#builds
 
 	def builds(self):
-		return self._api_call("/v1/builds", HttpMethod.GET, None)
+		return self._api_call("/v1/builds")
 
 	def builds_for_app(self, app_id):
-		return self._api_call("/v1/builds?filter[app]=" + app_id, HttpMethod.GET, None)
+		return self._api_call("/v1/builds?filter[app]=" + app_id)
 
 	def build_processing_state(self, app_id, version):
-		return self._api_call("/v1/builds?filter[app]=" + app_id + "&filter[version]=" + version + "&fields[builds]=processingState", HttpMethod.GET, None)
+		return self._api_call("/v1/builds?filter[app]=" + app_id + "&filter[version]=" + version + "&fields[builds]=processingState")
 
 	def set_uses_non_encryption_exemption_setting(self, build_id, uses_non_encryption_exemption_setting):
 		post_data = {'data': {'attributes': {'usesNonExemptEncryption': uses_non_encryption_exemption_setting}, 'id': build_id, 'type': 'builds'}}
@@ -120,10 +122,10 @@ class Api:
 	#betaBuildLocalizations
 
 	def beta_build_localizations_for_build(self, build_id):
-		return self._api_call("/v1/betaBuildLocalizations?filter[build]=" + build_id, HttpMethod.GET, None)
+		return self._api_call("/v1/betaBuildLocalizations?filter[build]=" + build_id)
 
 	def beta_build_localizations_for_build_and_locale(self, build_id, locale):
-		return self._api_call("/v1/betaBuildLocalizations?filter[build]=" + build_id + "&filter[locale]=" + locale, HttpMethod.GET, None)
+		return self._api_call("/v1/betaBuildLocalizations?filter[build]=" + build_id + "&filter[locale]=" + locale)
 
 	def create_beta_build_localization(self, build_id, locale, whatsNew):
 		post_data = {'data': { 'type': 'betaBuildLocalizations', 'relationships': {'build': {'data': {'id': build_id, 'type': 'builds'}}}, 'attributes': { 'locale': locale, 'whatsNew': whatsNew}}}
